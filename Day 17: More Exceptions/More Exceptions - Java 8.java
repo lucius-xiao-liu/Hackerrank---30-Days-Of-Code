8e98df03f9016bdf7b8707ce2d58b556184c2413
lucius-xiao-liu <51760338+lucius-xiao-liu@users.noreply.github.com>
2019-08-06 14:56:23 -0700

    Update More Exceptions - Java 8.java

--- Day 17: More Exceptions/More Exceptions - Java 8.java
+++ Day 17: More Exceptions/More Exceptions - Java 8.java
@@ -1,3 +1,7 @@
+/*
+ * For Loop
+*/
+
 class Calculator{
     int power(int n, int p) throws Exception {
         if (n<0 || p<0) {
@@ -5,6 +9,22 @@ class Calculator{
         } else{
         return (int)Math.pow(n,p);
         }
+    }
+
+}
+
+
+/*
+    * While Loop
+*/
+
+class Calculator{
+    int power(int n, int p) throws Exception {
+        while (n<0 || p<0) {
+            throw new Exception ("n and p should be non-negative");
+        } 
+        return (int)Math.pow(n,p);
+        
     }
 
 }
