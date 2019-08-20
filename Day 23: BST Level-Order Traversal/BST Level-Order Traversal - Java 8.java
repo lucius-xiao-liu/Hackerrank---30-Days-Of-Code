1881d27935e6d4cee0284ba2d1389db2da3a0dfa
lucius-xiao-liu <51760338+lucius-xiao-liu@users.noreply.github.com>
2019-08-20 11:10:17 -0700

    Update BST Level-Order Traversal - Java 8.java

--- Day 23: BST Level-Order Traversal/BST Level-Order Traversal - Java 8.java
+++ Day 23: BST Level-Order Traversal/BST Level-Order Traversal - Java 8.java
@@ -1,3 +1,28 @@
+/*
+levelOrder(BinaryTree t) {
+    if(t is not empty) {
+        // enqueue current root
+        queue.enqueue(t)
+        
+        // while there are nodes to process
+        while( queue is not empty ) {
+            // dequeue next node
+            BinaryTree tree = queue.dequeue();
+            
+            process tree's root;
+            
+            // enqueue child elements from next level in order
+            if( tree has non-empty left subtree ) {
+                queue.enqueue( left subtree of t )
+            }
+            if( tree has non-empty right subtree ) {
+                queue.enqueue( right subtree of t )
+            }
+        }
+    } 
+} 
+*/
+
 static void levelOrder(Node root){
       //Write your code here
       Queue<Node> tree = new LinkedList<>();
